1c27d1414da1ca4bbb62180d3498f71702b7e60d
Leonie <Lchamberlin-Medd@Outlook.com>
2024-10-10 14:28:03 +0100

    Updated IP

--- distributed-lab-1-master/client/client.go
+++ distributed-lab-1-master/client/client.go
@@ -30,7 +30,7 @@ func write(conn net.Conn) {
 
 func main() {
 	// Get the server address and port from the commandline arguments.
-	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
+	addrPtr := flag.String("ip", "50.19.58.224:8030", "IP:port string to connect to")
 	flag.Parse()
 	//TODO Try to connect to the server
 	conn, _ := net.Dial("tcp", *addrPtr)
